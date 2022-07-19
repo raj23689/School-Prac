7b2941137e565285304a9f5f567d861b2feed777
raj23689 <92665411+raj23689@users.noreply.github.com>
2022-07-19 19:15:16 +0530

    Added files and initialised the repo

---
+++ src/VolBox1.java
@@ -0,0 +1,8 @@
+public class VolBox1 {
+    double height, length, depth;
+    void Volume () {
+        double V = height * length * depth;
+        System.out.println("The volume is: " + V);
+    }
+
+}
