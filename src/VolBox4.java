2414383029b065e35d007c20411470853e400636
raj23689 <92665411+raj23689@users.noreply.github.com>
2022-07-20 09:48:36 +0530

    Added the main codes and methods

--- src/VolBox4.java
+++ src/VolBox4.java
@@ -4,4 +4,16 @@ public class VolBox4 {
     double Volume( double height, double width, double depth) {
         return height * width * depth;
     }
+
+    public static void main(String[] args) {
+        VolBox4 be1 = new VolBox4();
+        VolBox4 be2 = new VolBox4();
+        // box's data
+        double vol1 = be1.Volume(51,56, 48);
+        double vol2 = be2.Volume(45, 54, 23);
+
+        // printing the values
+        System.out.println("Box 1 vol: " + vol1);
+        System.out.println("Box 2 vol: " + vol2);
+    }
 }
