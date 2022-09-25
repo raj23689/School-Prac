6ce331ec1848c0ec7d35d4391b217366d8a1e209
raj23689 <92665411+raj23689@users.noreply.github.com>
2022-09-25 15:27:26 +0530

    Implemented class Box

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -1,2 +1,29 @@
+class Box {
+    private double width;
+    private double height;
+    private double length;
+    Box(double w, double h, double l) {
+        width = w;
+        height = h;
+        length = l;
+    }
+
+    Box() {
+        width = -1;
+        height  = -1;
+        length = -1;
+    }
+
+    Box(double len) {
+        width = height = length  = len;
+    }
+
+    double Vol() {
+        return height * length * width;
+    }
+}
+
+
+
 public class BoxDemo {
 }
