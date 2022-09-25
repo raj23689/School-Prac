5d18760f5e8d439c2c679693ac321df1c3f4c375
raj23689 <92665411+raj23689@users.noreply.github.com>
2022-09-25 15:05:34 +0530

    class Bat updated

--- src/Animals.java
+++ src/Animals.java
@@ -5,5 +5,17 @@ class Animal {
     }
 }
 
+class Bat extends Animal {
+    int k;
+    void ShowK() {
+        System.out.println("K : " + k);
+    }
+
+    @Override
+    void Show() {
+        System.out.println("i + j + k :: " + (i + j + k));
+    }
+}
+
 public class Animals {
 }
